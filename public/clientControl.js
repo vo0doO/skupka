7458b5fa06dae0dce5a556a52a8be1bfe8bbc360
vo0doo <danilakirsanovspb@gmail.com>
2018-04-11 00:42:28 +0300

    добавленна функция ауторизации через гугл

--- public/clientControl.js
+++ public/clientControl.js
@@ -36,3 +36,24 @@ $(() => {
         CI = ""
     });
 });
+
+var uiConfig = {
+    signInSuccessUrl: '/',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: 'https://xn----otbfcicqmv0g.xn--p1ai/'
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+$(() => {
+    $("#cl").on('click', () => {
+        $("#firebaseui-auth-container").toggle();
+    });
+});
